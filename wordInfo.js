b7e14284f2cf3aee859dd12c5613676deac9965a
Mattpott <mattpotter562@gmail.com>
2022-11-07 13:54:04 -0500

    setup

--- wordInfo.js
+++ wordInfo.js
@@ -7,3 +7,25 @@ variableSizeResults.forEach((result) => {
 });
 
 // assuming you already have rhyme results somewhere, for each of the first 10 results, query the word info api for the rhyming words' info and display them in a dl with that rhyming word
+
+async function begin() {
+  const rhymeResults = await fetch("example-rhyme-results.json");
+  const rhymeResultsJson = await rhymeResults.json();
+  console.log(rhymeResultsJson);
+  let i = 0;
+  const firstTen = rhymeResultsJson.map((elem) => {
+    if (i < 10) {
+      i++;
+      return elem.word;
+    }
+  });
+  console.log(firstTen);
+
+  // for (let i = 0; i < 10; i++) {
+  //   const cur = rhymeResultsJson.
+  //   wordDesc.push(await fetch(
+  //     // may need to replace with a JSON
+  //     `https://rhymebrain.com/talk?function=getWordInfo&word=${cur}`));
+  // }
+}
+begin();
